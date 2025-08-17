0716fe9c12b11322de6a5fd6b37445cff5ba0047
Magfur <41749833+magfurmtk@users.noreply.github.com>
2025-08-17 22:56:09 +0700

    Create utils.js

---
+++ src/utils.js
@@ -0,0 +1,9 @@
+import Union from "./contracts/Union.json";
+import UnionPool from "./contracts/UnionPool.json";
+
+// Ganti ini dengan alamat sebenarnya setelah deploy
+export const UNION_ADDRESS = "0xYourUnionTokenAddress";
+export const UNIONPOOL_ADDRESS = "0xYourUnionPoolAddress";
+
+export const UNION_ABI = Union.abi;
+export const UNIONPOOL_ABI = UnionPool.abi;
